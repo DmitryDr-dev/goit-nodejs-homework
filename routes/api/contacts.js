c92b80916c06912ad2db16be5c6d0444f1465874
DmitryDr-dev <dmitrydr.dev@gmail.com>
2022-01-03 19:21:40 +0200

    build: change cjs imports to es6 imports in contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,25 @@
-const express = require('express')
-const router = express.Router()
+import { Router } from 'express';
+
+const router = new Router();
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json({ message: 'template message' });
+});
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json({ message: 'template message' });
+});
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json({ message: 'template message' });
+});
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json({ message: 'template message' });
+});
 
 router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json({ message: 'template message' });
+});
 
-module.exports = router
+export default router;
