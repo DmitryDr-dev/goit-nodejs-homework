71b564343e2962ff65483866784b2d2eafe95d54
DmitryDr-dev <dmitrydr.dev@gmail.com>
2022-01-06 14:59:03 +0200

    feat: Add getContactById service function

---
+++ services/contacts/getContactById.js
@@ -0,0 +1,12 @@
+import Contact from '../../model/contact';
+import { ResultStatus } from '../../lib/constants';
+
+export async function getContactById(contactId) {
+  try {
+    const result = await Contact.findById(contactId);
+    return { status: ResultStatus.SUCCESS, data: result };
+  } catch (error) {
+    console.error(`Error occurred on getting contact by id`);
+    return { status: ResultStatus.ERROR, data: null };
+  }
+}
--- services/contacts/index.js
+++ services/contacts/index.js
@@ -1,7 +1,9 @@
 import { listContacts } from './listContacts';
+import { getContactById } from './getContactById';
 
 const contactsServices = {
   listContacts,
+  getContactById,
 };
 
 export default contactsServices;
