b58aaa03d1d9acf9ef99708a66f7aec5424d9587
DmitryDr-dev <dmitrydr.dev@gmail.com>
2022-01-07 12:47:08 +0200

    feat: Add contact validation schemas and functions

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,48 +1,18 @@
 import { Router } from 'express';
 import contactsControllers from '../../controllers/contacts';
-
-import model from '../../model/index.js';
-import { validateCreation, validateUpdate } from './validation';
+import {
+  validateCreation,
+  validateUpdate,
+  validateUpdateFavorite,
+} from '../../middlewares/validation/contactValidators';
 
 const router = new Router();
 
 router.get('/', contactsControllers.getContacts);
 router.get('/:contactId', contactsControllers.getContactById);
-router.post('/', contactsControllers.addContact);
+router.post('/', validateCreation, contactsControllers.addContact);
 router.delete('/:contactId', contactsControllers.removeContact);
-router.put('/:contactId', contactsControllers.updateContact);
-// router.get('/:contactId', async (req, res, next) => {
-//   const { contactId } = req.params;
-//   const result = await model.getContactById(contactId);
-//   if (!result) {
-//     res.status(404).json({ message: 'Not Found' });
-//   }
-
-//   res.status(200).json(result);
-// });
-
-// router.post('/', validateCreation, async (req, res, next) => {
-//   const newContact = await model.addContact(req.body);
-//   res.status(201).json(newContact);
-// });
-
-// router.delete('/:contactId', async (req, res, next) => {
-//   const { contactId } = req.params;
-//   const result = await model.removeContact(contactId);
-//   if (!result) {
-//     res.status(404).json({ message: 'Not Found' });
-//   }
-//   res.status(200).json({ message: 'Contact Deleted' });
-// });
-
-// router.put('/:contactId', validateUpdate, async (req, res, next) => {
-//   const { contactId } = req.params;
-//   const result = await model.updateContact(contactId, req.body);
-//   if (!result) {
-//     res.status(404).json({ message: 'Not Found' });
-//   }
-
-//   res.status(200).json(result);
-// });
+router.put('/:contactId', validateUpdate, contactsControllers.updateContact);
+router.patch('/:contactId/favorite', validateUpdateFavorite);
 
 export default router;
