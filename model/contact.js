1544aeca8e359bebb965b863b85a996f674b9074
DmitryDr-dev <dmitrydr.dev@gmail.com>
2022-01-07 13:21:40 +0200

    feat: Removing version key and adding virtual fields createdAt and updatedAt

--- model/contact.js
+++ model/contact.js
@@ -3,28 +3,42 @@ import { MIN_AGE, MAX_AGE } from '../lib/constants';
 
 const { Schema, model } = mongoose;
 
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, 'Set name for contact'],
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Set name for contact'],
+    },
+    age: {
+      type: Number,
+      min: MIN_AGE,
+      max: MAX_AGE,
+      default: null,
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
   },
-  age: {
-    type: Number,
-    min: MIN_AGE,
-    max: MAX_AGE,
-    default: null,
+  {
+    versionKey: false,
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+      transform: function (doc, ret) {
+        delete ret._id;
+        return ret;
+      },
+    },
+    toObject: { virtuals: true },
   },
-  email: {
-    type: String,
-  },
-  phone: {
-    type: String,
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
-});
+);
 
 const Contact = model('contact', contactSchema);
 
