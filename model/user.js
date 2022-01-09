ff0ee68957c207bc3251906d407cf4707cfa6f6d
DmitryDr-dev <dmitrydr.dev@gmail.com>
2022-01-09 09:56:14 +0200

    feat: Add user schema and complete contact schema

---
+++ model/user.js
@@ -0,0 +1,55 @@
+import mongoose from 'mongoose';
+import { Subscription } from '../lib/constants';
+
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      default: 'username',
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+      validate(value) {
+        const re =
+          /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/;
+        return re.test(String(value).trim().toLowerCase);
+      },
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+    },
+    subscription: {
+      type: String,
+      enum: {
+        values: Object.values(Subscription),
+        message: 'Invalid Subscription',
+      },
+      default: 'starter',
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+      transform: function (doc, ret) {
+        delete ret._id;
+        return ret;
+      },
+    },
+    toObject: { virtuals: true },
+  },
+);
+
+const User = model('user', userSchema);
+
+export default User;
