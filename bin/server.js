67e41e479a7ef8b8f8b0d1a437cffd497a74fcba
DmitryDr-dev <dmitrydr.dev@gmail.com>
2022-01-21 00:16:52 +0200

    feat: create folders on server startup and change user model settings

--- bin/server.js
+++ bin/server.js
@@ -1,10 +1,15 @@
 import app from '../app';
 import db from '../helper/db';
+import fs from 'fs/promises';
 
 const PORT = process.env.PORT || 3000;
+const UPLOAD_DIR = process.env.UPLOAD_DIR;
+const AVATAR_DIR = process.env.AVATAR_DIR;
 
 db.then(() => {
-  app.listen(PORT, () => {
+  app.listen(PORT, async () => {
+    await fs.mkdir(UPLOAD_DIR, { recursive: true });
+    await fs.mkdir(AVATAR_DIR, { recursive: true });
     console.log(`Server Running. Use our API on port: ${PORT}`);
   });
 }).catch(error => {
